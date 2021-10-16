ec58dd92d6fb3cb846870184ed55ed3851f4fbbe
GetGaming <emrekaplan12345678@gmail.com>
2021-10-16 09:38:14 +0200

    Initial commit

---
+++ Models/ItemModel.swift
@@ -0,0 +1,28 @@
+//
+//  ItemModel.swift
+//  ToDo
+//
+//  Created by Emre Kaplan on 03/10/2021.
+//
+
+import Foundation
+
+//Immutable Struct
+
+struct ItemModel: Identifiable, Codable {
+    let id: String
+    let title: String
+    let isCompleted: Bool
+    
+    init(id: String = UUID().uuidString, title: String, isCompleted: Bool) {
+        self.id = id
+        self.title = title
+        self.isCompleted = isCompleted
+    }
+    
+    func updateCompletion() -> ItemModel {
+        return ItemModel(id: id, title: title, isCompleted: !isCompleted)
+    }
+    
+}
+
---
+++ Views/ListView.swift
@@ -0,0 +1,45 @@
+//
+//  ListView.swift
+//  ToDo
+//
+//  Created by Emre Kaplan on 28/09/2021.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    
+    @EnvironmentObject var listViewModel: ListViewModel
+    
+    var body: some View {
+        List {
+            ForEach(listViewModel.items) { item in
+                ListRowView(item: item)
+                    .onTapGesture {
+                        withAnimation(.linear) {
+                            listViewModel.updateItem(item: item)
+                        }
+                    }
+            }
+            .onDelete(perform: listViewModel.deleteItem)
+            .onMove(perform: listViewModel.moveItem)
+        }
+        .listStyle(PlainListStyle())
+        .navigationTitle("ToDo List 📝")
+        .navigationBarItems(
+            leading: EditButton(),
+            trailing:
+                NavigationLink("Add", destination: AddView())
+            )
+    }
+    
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ListView()
+        }
+        .environmentObject(ListViewModel())
+    }
+}
